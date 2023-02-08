95864f6d3da820b2e99407786457fb69a363e394
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-08 11:26:03 +0330

    lendingpool address

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -8,16 +8,27 @@ async function main() {
 
     // Lending Pool Address Provider: 0x5E52dEc931FFb32f609681B8438A51c675cc232d
     // lending Pool : going to get from the Lending Pool Address Provider
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
 }
 
 async function getLendingPool(account) {
+    // create the lending pool address contract
     const lendingPoolAddressProvider = await ethers.getContractAt(
         "ILendingPoolAddressesProvider",
         "0x5E52dEc931FFb32f609681B8438A51c675cc232d",
         account
     )
 
+    // we get the lending pool address
     const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
+    // get the lending pool and return it
+    const lendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        account
+    )
+    return lendingPool
 }
 
 main()
